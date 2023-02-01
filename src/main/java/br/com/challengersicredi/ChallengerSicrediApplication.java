1b7ef8fd08f50c7ebb942038fa29a342c3560752
Tomas Fichtner <fichtner.dk@hotmail.com>
2023-02-01 09:07:28 -0300

    create-project

---
+++ src/main/java/br/com/challengersicredi/ChallengerSicrediApplication.java
@@ -0,0 +1,13 @@
+package br.com.challengersicredi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengerSicrediApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengerSicrediApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/challengersicredi/ChallengerSicrediApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.challengersicredi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengerSicrediApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
