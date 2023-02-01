c5c744fe07fc954a79be2ff1264f6d3456fab44c
Tomas Fichtner <fichtner.dk@hotmail.com>
2023-02-01 13:16:18 -0300

    fix::configuration for ci/cd for build

--- src/main/java/br/com/challengersicredi/ChallengerSicrediApplication.java
+++ src/main/java/br/com/challengersicredi/ChallengerSicrediApplication.java
@@ -1,9 +1,12 @@
 package br.com.challengersicredi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "br.com.challengersicredi")
 public class ChallengerSicrediApplication {
 
     public static void main(String[] args) {
