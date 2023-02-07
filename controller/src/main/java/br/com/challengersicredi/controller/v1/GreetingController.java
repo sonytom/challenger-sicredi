1d3e4d3ee0594adfce18f6f5c162ec0464dea709
Tomas Fichtner <fichtner.dk@hotmail.com>
2023-02-06 21:21:24 -0300

    set any configurations

--- controller/src/main/java/br/com/challengersicredi/controller/v1/GreetingController.java
+++ controller/src/main/java/br/com/challengersicredi/controller/v1/GreetingController.java
@@ -1,13 +1,18 @@
-package br.com.challengersicredi.controller;
+package br.com.challengersicredi.controller.v1;
 
+import br.com.challengersicredi.impl.GreetingService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class GreetingController {
+
+    GreetingService service;
     @GetMapping("/")
     public String greeting() {
-        return "teste";
+     return service.greeting();
     }
 
 }
