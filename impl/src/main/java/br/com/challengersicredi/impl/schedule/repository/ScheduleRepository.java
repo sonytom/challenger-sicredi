1480576d5f0e0502519f153236de9eb726fc266a
Tomas Fichtner <fichtner.dk@hotmail.com>
2023-02-14 09:02:57 -0300

    add:: find by id and name in repository

--- impl/src/main/java/br/com/challengersicredi/impl/schedule/repository/ScheduleRepository.java
+++ impl/src/main/java/br/com/challengersicredi/impl/schedule/repository/ScheduleRepository.java
@@ -1,6 +1,7 @@
 package br.com.challengersicredi.impl.schedule.repository;
 
 import br.com.challengersicredi.impl.schedule.entity.ScheduleModelEntity;
+import br.com.challengersicredi.impl.vote.entity.VoteModelEntity;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
@@ -8,5 +9,6 @@ import reactor.core.publisher.Mono;
 @Repository
 public interface ScheduleRepository extends ReactiveMongoRepository<ScheduleModelEntity, String> {
     Mono<ScheduleModelEntity> findByName(String name);
+    Mono<ScheduleModelEntity> findByIdAndName(String userID, String name);
 
 }
